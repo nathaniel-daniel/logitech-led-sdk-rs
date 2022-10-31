ac7cbf160f39670f907586127cae87a99175f8dc
nathaniel-daniel <80441888+nathaniel-daniel@users.noreply.github.com>
2022-10-31 16:29:25 -0700

    Update deps (#29)
    * Update deps
    
    * Upgrade deps

--- logitech-led-sdk-sys/build.rs
+++ logitech-led-sdk-sys/build.rs
@@ -3,7 +3,7 @@ use std::path::PathBuf;
 fn main() {
     let sdk_dir = std::env::var("LOGITECH_LED_SDK").expect("missing or invalid `LOGITECH_LED_SDK`");
     let include_dir = format!("{}//Include", sdk_dir);
-    let out_dir = std::env::var("OUT_DIR").expect("missing or invalid `OUT_DIR`");
+    let out_dir = std::env::var_os("OUT_DIR").expect("missing `OUT_DIR`");
     let out_path = PathBuf::from(out_dir);
 
     if std::env::var_os("CARGO_CFG_WINDOWS").is_none() {
@@ -32,7 +32,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_arg("-xc++")
-        .clang_arg(&format!("-I{}", include_dir))
+        .clang_arg(format!("-I{}", include_dir))
         .allowlist_type("LogiLed::.*")
         .allowlist_function("Logi.*")
         .allowlist_var(".*")
