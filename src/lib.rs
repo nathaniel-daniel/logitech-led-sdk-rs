c79016de9e5b24743726c8ceaf9dd08f560ac0c5
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-06-05 19:07:50 -0700

    Rework api

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,7 @@ pub use self::color_percent::ColorPercent;
 pub use self::sdk::Sdk;
 pub use self::target_device::TargetDevice;
 pub use logitech_led_sdk_sys as sys;
-use std::convert::TryInto;
-use std::os::raw::c_int;
 use std::sync::Mutex;
-use std::time::Duration;
 pub use sys::LogiLed_DeviceType as DeviceType;
 pub use sys::LogiLed_KeyName as KeyName;
 
@@ -18,177 +15,6 @@ pub use sys::LogiLed_KeyName as KeyName;
 /// If you use raw sdk api functions anywhere, you MUST use this lock to wrap accesses to the sdk in order to prevent data races.
 /// This library does all this for you, this is exposed only for users who want to use raw sdk functions safely.
 pub static SDK_LOCK: Mutex<()> = Mutex::new(());
-
-impl Sdk {
-    /// Sets the lighting for a keyboard key by scan code
-    pub fn set_lighting_for_key_with_scan_code(&self, code: u32, color: ColorPercent) -> bool {
-        unsafe {
-            sys::LogiLedSetLightingForKeyWithScanCode(
-                code as _,
-                color.r as _,
-                color.g as _,
-                color.b as _,
-            )
-        }
-    }
-
-    /// Sets the lighting for a keyboard key by HID code
-    pub fn set_lighting_for_key_with_hid_code(&self, code: u32, color: ColorPercent) -> bool {
-        unsafe {
-            sys::LogiLedSetLightingForKeyWithHidCode(
-                code as _,
-                color.r as _,
-                color.g as _,
-                color.b as _,
-            )
-        }
-    }
-
-    /// Sets the lighting for a specific device's target zone
-    pub fn set_lighting_for_target_zone(
-        &self,
-        device: DeviceType,
-        zone: usize,
-        color: ColorPercent,
-    ) -> bool {
-        unsafe {
-            sys::LogiLedSetLightingForTargetZone(
-                device as _,
-                zone as _,
-                color.r as _,
-                color.g as _,
-                color.b as _,
-            )
-        }
-    }
-
-    /// Returns None if the call fails or any of the time values are too large. Duration how long the flashes occur overall. The interval is the time between flashes.
-    pub fn flash_lighting(
-        &self,
-        color: ColorPercent,
-        duration: Duration,
-        interval: Duration,
-    ) -> bool {
-        let duration: c_int = match duration.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        let interval: c_int = match interval.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        unsafe {
-            sys::LogiLedFlashLighting(color.r as _, color.g as _, color.b as _, duration, interval)
-        }
-    }
-
-    /// Returns None if the call fails or any of the time values are too large. Duration how long the flashes occur overall. The interval is the time between flashes.
-    pub fn flash_single_key(
-        &self,
-        key: KeyName,
-        color: ColorPercent,
-        duration: Duration,
-        interval: Duration,
-    ) -> bool {
-        let duration: c_int = match duration.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        let interval: c_int = match interval.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        unsafe {
-            sys::LogiLedFlashSingleKey(
-                key as _,
-                color.r as _,
-                color.g as _,
-                color.b as _,
-                duration,
-                interval,
-            )
-        }
-    }
-
-    /// Stops all current LED effects
-    pub fn stop_effects(&self) -> bool {
-        unsafe { sys::LogiLedStopEffects() }
-    }
-
-    /// Stops all LED effects on one key
-    pub fn stop_effects_on_key(&self, key: KeyName) -> bool {
-        unsafe { sys::LogiLedStopEffectsOnKey(key as _) }
-    }
-
-    /// Returns None if the call fails or any of the time values are too large.
-    ///
-    /// Duration is how long the pulses occur overall.
-    /// The interval is the time between pulses.
-    pub fn pulse_lighting(
-        &self,
-        color: ColorPercent,
-        duration: Duration,
-        interval: Duration,
-    ) -> bool {
-        let duration: c_int = match duration.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        let interval: c_int = match interval.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        unsafe {
-            sys::LogiLedPulseLighting(color.r as _, color.g as _, color.b as _, duration, interval)
-        }
-    }
-
-    /// Returns None if the call fails or any of the time values are too large. Duration how long the pulses occur overall.
-    pub fn pulse_single_key(
-        &self,
-        key: KeyName,
-        start_color: ColorPercent,
-        end_color: ColorPercent,
-        duration: Duration,
-        is_infinite: bool,
-    ) -> bool {
-        let duration: c_int = match duration.as_millis().try_into() {
-            Ok(v) => v,
-            Err(_e) => return false,
-        };
-        unsafe {
-            sys::LogiLedPulseSingleKey(
-                key as _,
-                start_color.r as _,
-                start_color.g as _,
-                start_color.b as _,
-                end_color.r as _,
-                end_color.g as _,
-                end_color.b as _,
-                duration,
-                is_infinite,
-            )
-        }
-    }
-
-    /// Saves the current lighting config for the given key
-    pub fn save_lighting_for_key(&self, key: KeyName) -> bool {
-        unsafe { sys::LogiLedSaveLightingForKey(key as _) }
-    }
-
-    /// Saves the current lighting config for the given key
-    pub fn restore_lighting_for_key(&self, key: KeyName) -> bool {
-        unsafe { sys::LogiLedRestoreLightingForKey(key as _) }
-    }
-}
-
-impl Drop for Sdk {
-    fn drop(&mut self) {
-        unsafe {
-            sys::LogiLedShutdown();
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
@@ -222,13 +48,13 @@ mod test {
         ));
         assert!(sdk.flash_lighting(
             ColorPercent::new_rgb(255, 0, 0),
-            Duration::from_millis(10_000),
+            Some(Duration::from_millis(10_000)),
             Duration::from_millis(100)
         ));
         assert!(sdk.stop_effects());
         assert!(sdk.pulse_lighting(
             ColorPercent::new_rgb(255, 0, 0),
-            Duration::from_millis(10_000),
+            Some(Duration::from_millis(10_000)),
             Duration::from_millis(100)
         ));
         assert!(sdk.stop_effects());
@@ -239,7 +65,7 @@ mod test {
         assert!(sdk.flash_single_key(
             KeyName::L,
             ColorPercent::new_rgb(255, 0, 0),
-            Duration::from_millis(10_000),
+            Some(Duration::from_millis(10_000)),
             Duration::from_millis(100)
         ));
         assert!(sdk.pulse_single_key(
--- src/sdk.rs
+++ src/sdk.rs
@@ -1,11 +1,14 @@
 use crate::sys;
 use crate::ColorPercent;
+use crate::DeviceType;
 use crate::KeyName;
 pub use crate::TargetDevice;
 use crate::SDK_LOCK;
 use std::ffi::CString;
+use std::os::raw::c_int;
 use std::sync::MutexGuard;
 use std::sync::TryLockError;
+use std::time::Duration;
 
 /// Entry to Api.
 ///
@@ -80,7 +83,7 @@ impl Sdk {
     /// Returns true if the target devices were selected.
     pub fn set_target(&self, target_device: TargetDevice) -> bool {
         // u32 -> i32, transmute
-        unsafe { sys::LogiLedSetTargetDevice(target_device.bits() as i32) }
+        unsafe { sys::LogiLedSetTargetDevice(target_device.bits() as c_int) }
     }
 
     /// Sets the lighting.
@@ -98,11 +101,245 @@ impl Sdk {
     pub fn set_lighting_for_key_with_name(&self, key: KeyName, color: ColorPercent) -> bool {
         unsafe {
             sys::LogiLedSetLightingForKeyWithKeyName(
-                key as _,
+                key,
                 color.r.into(),
                 color.g.into(),
                 color.b.into(),
             )
+        }
+    }
+
+    /// Sets the lighting for a keyboard key by scan code.
+    ///
+    /// # Returns
+    /// Returns true if successful.
+    pub fn set_lighting_for_key_with_scan_code(&self, scan_code: u32, color: ColorPercent) -> bool {
+        unsafe {
+            sys::LogiLedSetLightingForKeyWithScanCode(
+                scan_code as c_int,
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+            )
+        }
+    }
+
+    /// Sets the lighting for a keyboard key by HID code.
+    ///
+    /// # Returns
+    /// Returns true if successful.
+    pub fn set_lighting_for_key_with_hid_code(&self, hid_code: u32, color: ColorPercent) -> bool {
+        unsafe {
+            sys::LogiLedSetLightingForKeyWithHidCode(
+                hid_code as c_int,
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+            )
+        }
+    }
+
+    /// Sets the lighting for a specific device's target zone.
+    ///
+    /// A zone number is generally different per device, read the offical SDK docs for more info.
+    ///
+    /// # Returns
+    /// Returns true if successful.
+    pub fn set_lighting_for_target_zone(
+        &self,
+        device: DeviceType,
+        zone: u32,
+        color: ColorPercent,
+    ) -> bool {
+        unsafe {
+            sys::LogiLedSetLightingForTargetZone(
+                device,
+                zone as c_int,
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+            )
+        }
+    }
+
+    /// Save the current lighting, play the effect, and restore the lighting.
+    ///
+    /// duration controls how long the flashes occur overall.
+    /// If omitted, it will run until manually stopped.
+    /// The interval is the time between flashes.
+    ///
+    /// # Returns
+    /// Returns false if the call fails or any of the time values are too large.
+    pub fn flash_lighting(
+        &self,
+        color: ColorPercent,
+        duration: Option<Duration>,
+        interval: Duration,
+    ) -> bool {
+        let duration = match duration.map(|duration| c_int::try_from(duration.as_millis())) {
+            Some(Ok(duration)) if duration != sys::LOGI_LED_DURATION_INFINITE as c_int => duration,
+            Some(Err(_)) | Some(Ok(_)) => return false,
+            None => sys::LOGI_LED_DURATION_INFINITE as c_int,
+        };
+        let interval = match c_int::try_from(interval.as_millis()) {
+            Ok(interval) => interval,
+            Err(_) => return false,
+        };
+
+        unsafe {
+            sys::LogiLedFlashLighting(
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+                duration,
+                interval,
+            )
+        }
+    }
+
+    /// Start a flashing effect on the given key.
+    ///
+    /// duration controls how long the flashes occur overall.
+    /// If omitted, it will run until manually stopped.
+    /// The interval is the time between flashes.
+    ///
+    /// # Returns
+    /// Returns false if the call fails or any of the time values are too large.
+    pub fn flash_single_key(
+        &self,
+        key: KeyName,
+        color: ColorPercent,
+        duration: Option<Duration>,
+        interval: Duration,
+    ) -> bool {
+        let duration = match duration.map(|duration| c_int::try_from(duration.as_millis())) {
+            Some(Ok(duration)) if duration != sys::LOGI_LED_DURATION_INFINITE as c_int => duration,
+            Some(Err(_)) | Some(Ok(_)) => return false,
+            None => sys::LOGI_LED_DURATION_INFINITE as c_int,
+        };
+        let interval = match c_int::try_from(interval.as_millis()) {
+            Ok(interval) => interval,
+            Err(_) => return false,
+        };
+
+        unsafe {
+            sys::LogiLedFlashSingleKey(
+                key,
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+                duration,
+                interval,
+            )
+        }
+    }
+
+    /// Stops all current LED effects.
+    ///
+    /// # Returns
+    /// Returns false if the call fails.
+    pub fn stop_effects(&self) -> bool {
+        unsafe { sys::LogiLedStopEffects() }
+    }
+
+    /// Stops all LED effects on one key.
+    ///
+    /// # Returns
+    /// Returns false if the call fails.
+    pub fn stop_effects_on_key(&self, key: KeyName) -> bool {
+        unsafe { sys::LogiLedStopEffectsOnKey(key) }
+    }
+
+    /// Save the current lighting, pulse the lighting, then restore the lighting.
+    ///
+    /// duration controls how long the pulses occur overall.
+    /// If omitted, it will run until manually stopped.
+    /// The interval is the time between pulses.
+    ///
+    /// # Returns
+    /// Returns false if the call fails or any of the time values are too large.
+    pub fn pulse_lighting(
+        &self,
+        color: ColorPercent,
+        duration: Option<Duration>,
+        interval: Duration,
+    ) -> bool {
+        let duration = match duration.map(|duration| c_int::try_from(duration.as_millis())) {
+            Some(Ok(duration)) if duration != sys::LOGI_LED_DURATION_INFINITE as c_int => duration,
+            Some(Err(_)) | Some(Ok(_)) => return false,
+            None => sys::LOGI_LED_DURATION_INFINITE as c_int,
+        };
+        let interval = match c_int::try_from(interval.as_millis()) {
+            Ok(interval) => interval,
+            Err(_) => return false,
+        };
+
+        unsafe {
+            sys::LogiLedPulseLighting(
+                color.r.into(),
+                color.g.into(),
+                color.b.into(),
+                duration,
+                interval,
+            )
+        }
+    }
+
+    /// Start a pulsing effect on the given key.
+    ///
+    /// duration controls how long the pulses occur overall.
+    ///
+    /// # Returns
+    /// Returns false if the call fails or any of the time values are too large.
+    pub fn pulse_single_key(
+        &self,
+        key: KeyName,
+        start_color: ColorPercent,
+        end_color: ColorPercent,
+        duration: Duration,
+        is_infinite: bool,
+    ) -> bool {
+        let duration = match c_int::try_from(duration.as_millis()) {
+            Ok(duration) => duration,
+            Err(_) => return false,
+        };
+
+        unsafe {
+            sys::LogiLedPulseSingleKey(
+                key,
+                start_color.r.into(),
+                start_color.g.into(),
+                start_color.b.into(),
+                end_color.r.into(),
+                end_color.g.into(),
+                end_color.b.into(),
+                duration,
+                is_infinite,
+            )
+        }
+    }
+
+    /// Saves the current lighting config for the given key.
+    ///
+    /// # Returns
+    /// Returns false if the call fails.
+    pub fn save_lighting_for_key(&self, key: KeyName) -> bool {
+        unsafe { sys::LogiLedSaveLightingForKey(key) }
+    }
+
+    /// Restores the current lighting config for the given key.
+    ///
+    /// # Returns
+    /// Returns false if the call fails.
+    pub fn restore_lighting_for_key(&self, key: KeyName) -> bool {
+        unsafe { sys::LogiLedRestoreLightingForKey(key) }
+    }
+}
+
+impl Drop for Sdk {
+    fn drop(&mut self) {
+        unsafe {
+            sys::LogiLedShutdown();
         }
     }
 }
