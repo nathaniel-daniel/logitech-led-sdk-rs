ec91953f6c45fe65b197ab3559221a0983274356
nathaniel-daniel <80441888+nathaniel-daniel@users.noreply.github.com>
2023-06-05 17:14:34 -0700

    Remove bindgen (#44)
    * Remove bindgen
    
    * Update CI

--- logitech-led-sdk-sys/build.rs
+++ logitech-led-sdk-sys/build.rs
@@ -1,14 +1,5 @@
-use std::path::PathBuf;
-
 fn main() {
     let sdk_dir = std::env::var("LOGITECH_LED_SDK").expect("missing or invalid `LOGITECH_LED_SDK`");
-    let include_dir = format!("{sdk_dir}//Include");
-    let out_dir = std::env::var_os("OUT_DIR").expect("missing `OUT_DIR`");
-    let out_path = PathBuf::from(out_dir);
-
-    if std::env::var_os("CARGO_CFG_WINDOWS").is_none() {
-        panic!("This library will only work on Windows");
-    }
 
     match std::env::var("CARGO_CFG_TARGET_ARCH")
         .expect("missing or invalid `CARGO_CFG_TARGET_ARCH`")
@@ -24,23 +15,5 @@ fn main() {
             panic!("Arch \"{arch}\" is not supported");
         }
     };
-
-    println!("cargo:rerun-if-changed=wrapper.h");
-    println!("cargo:rerun-if-env-changed=LOGITECH_LED_SDK");
     println!("cargo:rustc-link-lib=LogitechLEDLib");
-
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg("-xc++")
-        .clang_arg(format!("-I{include_dir}"))
-        .allowlist_type("LogiLed::.*")
-        .allowlist_function("Logi.*")
-        .allowlist_var(".*")
-        .rustified_enum("LogiLed::.*")
-        .generate()
-        .expect("unable to generate bindings");
-
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("failed to write bindings");
 }
--- logitech-led-sdk-sys/src/lib.rs
+++ logitech-led-sdk-sys/src/lib.rs
@@ -3,4 +3,17 @@
 #![allow(non_snake_case)]
 #![allow(clippy::unreadable_literal)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(not(target_os = "windows"))]
+compile_error!("this library will only work on Windows");
+
+#[cfg(not(target_env = "msvc"))]
+compile_error!("this library will only work on the MSVC target environment");
+
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86")))]
+compile_error!("this library will only work on x86 or x86_64");
+
+#[cfg(target_arch = "x86_64")]
+include!("bindings-x86_64.rs");
+
+#[cfg(target_arch = "x86")]
+include!("bindings-i686.rs");
